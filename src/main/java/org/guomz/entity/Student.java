f5e1944d49d147f7ecff9f96f9bb416b9cf93d67
guomz <1258746567@qq.com>
2021-08-02 09:35:41 +0800

    first commit

---
+++ src/main/java/org/guomz/entity/Student.java
@@ -0,0 +1,33 @@
+package org.guomz.entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    private String name;
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/org/guomz/service/MyService.java
@@ -0,0 +1,8 @@
+package org.guomz.service;
+
+import org.guomz.entity.Student;
+
+public interface MyService {
+    String sayHello();
+    Student getStudent();
+}
